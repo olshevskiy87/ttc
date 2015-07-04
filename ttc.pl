2ba322101c59d43df9c313a6788061ef24bfe143
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2015-07-04 12:55:36 +0300

    line numbers in the output results; [\g]lobal search for each line

--- ttc.pl
+++ ttc.pl
@@ -10,25 +10,28 @@ my $ww_file = 'wrong_words';
 
 unless (-e $ww_file) {
     croak "file $ww_file does not exist!\n";
-    exit 0;
+    exit 1;
 }
 
 open my $fh, '<', $ww_file or croak $ERRNO;
 my @wrong_words = <$fh>;
 close $fh;
-chomp @wrong_words;
-@wrong_words = grep {/\w/x} @wrong_words;
 
+chomp @wrong_words;
+@wrong_words = grep { ! /^$/ } @wrong_words;
 my $chain = join('|', @wrong_words);
 
-my $pattern;
+my ($pattern, @matches);
 while (<>) {
     chomp;
-    if (/\b($chain)\b/x) {
-        printf qq{%s: [%s] "%s"\n}
-            , colored($ARGV, 'yellow')
-            , colored($1, 'red')
-            , $_;
+    if (@matches = $_ =~ /\b($chain)\b/g) {
+        foreach my $match (@matches) {
+            printf qq{%s:%s [%s] "%s"\n}
+                , colored($ARGV, 'yellow')
+                , $NR
+                , colored($match, 'red')
+                , $_;
+        }
     }
 }
 
